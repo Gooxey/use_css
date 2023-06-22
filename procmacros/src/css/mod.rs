f2ed7878aced397c8d6c745a155cd634e809a000
Gooxey <dennis.tiderko@gmail.com>
2023-06-22 22:58:54 +0200

    Fixed nested styles and added some other features

--- procmacros/src/css/mod.rs
+++ procmacros/src/css/mod.rs
@@ -9,11 +9,11 @@ pub struct Css {
     rulesets: Vec<RuleSet>,
 }
 impl Css {
+    /// Generate rust functions from the parsed css code.
     pub fn to_rust(&self) -> TokenStream {
         let mut rust_code = quote!();
         for ruleset in &self.rulesets {
             let ruleset_rust_code = ruleset.to_rust();
-
             rust_code = quote!(
                 #rust_code
                 #ruleset_rust_code
@@ -25,30 +25,29 @@ impl Css {
 }
 impl From<String> for Css {
     fn from(value: String) -> Self {
-        let css_code = value.split('\n');
-
         // Find Rulesets in the given css_code
-        // Then let these Rulesets parse their Rules
         let mut rulesets = vec![];
         let mut current_ruleset: Vec<&str> = vec![];
-        // required for parsing nested rulesets
+        // we need this counter to allow the use of nested styles
         let mut parsing_level = 0;
-        for line in css_code {
-            if line.contains('{') {
-                parsing_level += 1;
-
-                if parsing_level == 1 {
-                    current_ruleset.clear();
-                }
-            } else if line.contains('}') {
-                parsing_level -= 1;
-
-                if parsing_level == 0 {
-                    rulesets.push(RuleSet::from(current_ruleset.clone()));
-                    continue;
-                }
+        for line in value.split('\n') {
+            let line = line.trim();
+            if line.is_empty() {
+                continue;
             }
             current_ruleset.push(line);
+
+            if line.contains('{') {
+                parsing_level += 1;
+                continue;
+            } else if line.contains('}') {
+                parsing_level -= 1;
+            }
+
+            if parsing_level == 0 {
+                rulesets.push(RuleSet::from(current_ruleset.clone()));
+                current_ruleset.clear();
+            }
         }
 
         Self { rulesets }
